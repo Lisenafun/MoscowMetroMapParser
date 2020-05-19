a5cbe30fc6de98a308f5861e8acd40cf09a3ee6c
OlesiaViazankina <lisena.cmex@rambler.ru>
2020-05-19 17:31:30 +0300

    homework 4.2 done

---
+++ 04_NumbersStringsAndDates/DigitsMinMax/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int maxInt = Integer.MAX_VALUE;
+        int minInt = Integer.MIN_VALUE;
+        short maxShort = Short.MAX_VALUE;
+        short minShort = Short.MIN_VALUE;
+        long maxLong = Long.MAX_VALUE;
+        long minLong = Long.MIN_VALUE;
+        double maxDouble = Double.MAX_VALUE;
+        double minDouble = Double.MIN_VALUE;
+        float maxFloat = Float.MAX_VALUE;
+        float minFloat = Float.MIN_VALUE;
+
+        System.out.println ("maxInt: " + maxInt + "\n" + "minInt: " + minInt + "\n"
+                + "maxShort: " + maxShort + "\n" + "maxShort: " + minShort + "\n"
+                + "maxLong: " + maxLong + "\n" + "maxLong: " + minLong + "\n"
+                + "maxDouble: " + maxDouble + "\n" + "maxDouble: " + minDouble + "\n"
+                + "maxFloat: " + maxFloat + "\n" + "maxFloat: " + minFloat);
+    }
+}
